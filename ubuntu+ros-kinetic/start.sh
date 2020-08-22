5be463d23830417e0e4ddc09e518f86c3137aa20
seigot <you@example.com>
2020-08-23 00:59:42 +0900

    update ubuntu+ros-kinetic environment

--- ubuntu+ros-kinetic/start.sh
+++ ubuntu+ros-kinetic/start.sh
@@ -19,6 +19,72 @@ function install_package(){
 }
 install_package
 
+function install_package2(){
+    # bocchi
+    #pip install numpy==1.16.6 \
+    #    scipy==1.2.1 \
+    #    gym==0.16.0 \
+    #    Markdown==3.1.1 \
+    #    setuptools==44.1.1 \
+    #    grpcio==1.27.2 \
+    #    mock==3.0.5 \
+    #    tensorflow==1.14.0 \
+    #    tensorflow-gpu==1.14.0 \
+    #    keras==2.3.0 \
+    #    flatten_json==0.1.7
+    sudo apt-get install -y ros-kinetic-opencv-apps
+    
+    # maru-mi
+    #pip install \
+    #	#Keras==2.2.4 \
+    #	keras-rl==0.4.2 \
+    #	matplotlib==2.2.5 \
+    #	opencv-python==4.2.0.32
+    #numpy==1.16.6 \
+	
+    #tensorflow==1.14.0
+    
+    # AI
+    sudo apt install -y ros-kinetic-dwa-local-planner
+    pip install tensorflow==1.5.0 keras==2.2.5
+    
+    # Bola de arroz
+    sudo apt install -y ros-kinetic-libg2o
+    sudo apt install -y libopencv-dev
+    sudo apt install -y ros-kinetic-costmap-converter
+    sudo apt install -y libsuitesparse-dev
+    sudo apt install -y libarmadillo-dev libarmadillo6
+    
+    # ikepoyo
+    sudo apt-get install -y ros-kinetic-dwa-local-planner
+    sudo apt-get install -y libarmadillo-dev libarmadillo6
+    
+    # gantetsu
+    sudo apt-get install -y ros-kinetic-dwa-local-planner
+    sudo apt-get install -y ros-kinetic-slam-gmapping
+    pip install transitions
+    sudo apt-get install -y graphviz graphviz-dev
+    pip install graphviz
+    sudo apt install -y ros-kinetic-executive-smach
+    rospack find smach
+    sudo apt-get install --no-install-recommends -y libarmadillo-dev libarmadillo6
+    
+    # daito
+    sudo apt-get install -y ros-kinetic-dwa-local-planner 
+    sudo apt-get install -y ros-kinetic-jsk-visualization
+    
+    # anko
+    sudo apt-get install -y ros-kinetic-dwa-local-planner ros-kinetic-geometry2
+    
+    # indo carry
+    #pip install opencv-python==4.2.0.32
+    sudo apt-get install -y ros-kinetic-vision-opencv
+    sudo apt-get install -y python-opencv
+    sudo apt-get install -y libopencv-dev
+    sudo apt-get install -y ros-kinetic-cv-camera
+}
+install_package2
+
 # workspace作成
 #mkdir -p $HOME/catkin_ws/src
 #cd $HOME/catkin_ws/src
@@ -49,8 +115,10 @@ source $HOME/.bashrc
 mkdir -p $HOME/catkin_ws/src
 cd $HOME/catkin_ws/src
 git clone https://github.com/pal-robotics/aruco_ros   # arco
-git clone https://github.com/OneNightROBOCON/burger_war # onenightrobocon
+#git clone https://github.com/OneNightROBOCON/burger_war # onenightrobocon
+git clone https://github.com/seigot/burger_war
 git clone https://github.com/tysik/obstacle_detector.git # obstacle detector
+git clone https://github.com/seigot/burger_war_autotest2
 cd $HOME/catkin_ws
 catkin build
 echo "source ~/catkin_ws/devel/setup.bash" >> $HOME/.bashrc
