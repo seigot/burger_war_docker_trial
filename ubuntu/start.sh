cc37793b8dbfa64bf7f47c1eedd581cb579fea95
seigot <you@example.com>
2021-01-31 22:43:27 +0900

    update ubuntu18.04 Dockerfile

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -10,70 +10,52 @@ function install_package(){
     sudo apt-get install -y apt-utils
     sudo apt-get install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential python-pip ros-melodic-turtlebot3 ros-melodic-turtlebot3-msgs ros-melodic-turtlebot3-simulations
     pip install requests flask
-    sudo apt-get install -y ros-melodic-dwa-local-planner
-    sudo apt-get install -y ros-melodic-global-planner
 
-    ###--->
-    sudo apt-get install -y ros-melodic-dwa-local-planner
-    sudo apt-get install -y ros-melodic-slam-gmapping
+    # my repository
+    sudo apt-get install ros-melodic-dwa-local-planner
+    sudo apt-get install ros-melodic-global-planner
+    sudo apt install -y libarmadillo-dev libarmadillo8  # necessary to install obstacle_detector
+
+    # other repository --->
+    sudo apt-get install ros-melodic-dwa-local-planner
+    sudo apt-get install ros-melodic-slam-gmapping
     pip install transitions
-    sudo apt-get install -y graphviz graphviz-dev
-    pip install pygraphviz
-    #sudo apt-get install -y ros-groovy-executive-smach
-    #rospack find smach
-    sudo apt-get install --no-install-recommends -y libarmadillo-dev
-    #cd ~/catkin_ws/src
-    
+    sudo apt-get install graphviz graphviz-dev
+    pip install graphviz
+    sudo apt install ros-melodic-executive-smach
+    sudo apt-get install --no-install-recommends -y libarmadillo-dev libarmadillo8
+
     ###
-    sudo apt install -y ros-melodic-dwa-local-planner
     sudo apt install -y ros-melodic-jsk-rviz-plugins
     sudo apt install -y ros-melodic-smach*
-    sudo apt install -y libarmadillo-dev #libarmadillo6
-    
+
     ###
     sudo apt install -y ros-melodic-libg2o
     sudo apt install -y libopencv-dev
     sudo apt install -y ros-melodic-costmap-converter
     sudo apt install -y libsuitesparse-dev
-    sudo apt install -y libarmadillo-dev #libarmadillo6
-    ###--->
+    sudo apt install -y libarmadillo-dev libarmadillo8
+    ### --->
 }
 install_package
 
-# workspace作成
-#mkdir -p $HOME/catkin_ws/src
-#cd $HOME/catkin_ws/src
-#catkin_init_workspace
-#cd $HOME/catkin_ws/
-#catkin_make
-#echo "source ~/catkin_ws/devel/setup.bash" >> $HOME/.bashrc
-#source $HOME/.bashrc
-
 # Turtlebot3のモデル名の指定を環境変数に追加。
-echo "export GAZEBO_MODEL_PATH=$HOME/catkin_ws/src/burger_war/burger_war/models/" >> $HOME/.bashrc
+# echo "export GAZEBO_MODEL_PATH=$HOME/catkin_ws/src/burger_war/burger_war/models/" >> $HOME/.bashrc
 echo "export TURTLEBOT3_MODEL=burger" >> $HOME/.bashrc
 source $HOME/.bashrc
 
-# make
-#cd $HOME/catkin_ws/src
-#git clone https://github.com/pal-robotics/aruco_ros
-#cd $HOME/catkin_ws
-#catkin_make
-
-# onenightrobocon
-#cd $HOME/catkin_ws/src
-#git clone https://github.com/OneNightROBOCON/burger_war
-#mv burger_war burger_war.org
-#cd $HOME/catkin_ws
-#catkin_make
-
+# workspace作成
+# build
 mkdir -p $HOME/catkin_ws/src
 cd $HOME/catkin_ws/src
-git clone https://github.com/pal-robotics/aruco_ros   # arco
-git clone https://github.com/OneNightROBOCON/burger_war # onenightrobocon
+#git clone https://github.com/pal-robotics/aruco_ros   # arco
+git clone https://github.com/seigot/burger_war_kit
+git clone https://github.com/seigot/burger_war_dev
 git clone https://github.com/tysik/obstacle_detector.git # obstacle detector
 cd $HOME/catkin_ws
+catkin init
 catkin build
+
 echo "source ~/catkin_ws/devel/setup.bash" >> $HOME/.bashrc
 source $HOME/.bashrc
 
